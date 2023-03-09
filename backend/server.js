0198e37a628a83e3b3f45d74e2fd63aa33fdbde2
M1j01337 <mijo.markovic139@gmail.com>
2023-03-09 21:23:17 +0000

    Established connection with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("hello")
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const colors = require('colors');
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
